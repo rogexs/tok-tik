7246af8973ded01f0c87025818aeb3ed404f1895
Rogelio Santiago Celaya <tecmobot@gmail.com>
2024-01-03 23:13:14 -0600

    Primer Commit

---
+++ lib/shared/data/local_video_posts.dart
@@ -0,0 +1,32 @@
+List<Map<String, dynamic>> videoPosts = [
+  {
+    'name': 'La carretera se hace larga y yo siento que puedo morir',
+    'videoUrl': 'assets/videos/1.mp4',
+    'likes': 17234,
+    'views': 1523,
+  },
+  {
+    'name': 'Al infinito y mas alla',
+    'videoUrl': 'assets/videos/2.mp4',
+    'likes': 45648,
+    'views': 1343,
+  },
+  {
+    'name': 'Viva gamers life',
+    'videoUrl': 'assets/videos/3.mp4',
+    'likes': 32454,
+    'views': 123563,
+  },
+  {
+    'name': 'En el mar la vida es mas sabrosa',
+    'videoUrl': 'assets/videos/4.mp4',
+    'likes': 3342,
+    'views': 2300,
+  },
+  {
+    'name': 'Feliz año nuevo',
+    'videoUrl': 'assets/videos/5.mp4',
+    'likes': 12334,
+    'views': 31030,
+  },
+];
